2ed97cc299bf03948bb3f94efdcd79ddfcd52914
Your Name <you@example.com>
2025-02-04 21:30:22 +0600

    edited code structure

--- client/src/pages/Dashboard/Admin/ManageUsers.jsx
+++ client/src/pages/Dashboard/Admin/ManageUsers.jsx
@@ -1,70 +1,79 @@
-import { Helmet } from 'react-helmet-async'
-import UserDataRow from '../../../components/Dashboard/TableRows/UserDataRow'
-import { useQuery } from '@tanstack/react-query'
-import useAxiosSecure from '../../../hooks/useAxiosSecure'
-import LoadingSpinner from '../../../components/Shared/LoadingSpinner'
-import { useContext } from 'react'
-import { AuthContext } from '../../../providers/AuthProvider'
+import { Helmet } from "react-helmet-async";
+import UserDataRow from "../../../components/Dashboard/TableRows/UserDataRow";
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import LoadingSpinner from "../../../components/Shared/LoadingSpinner";
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProvider";
 
 const ManageUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useContext(AuthContext);
 
-  const axiosSecure=useAxiosSecure();
-  const{user}=useContext(AuthContext)
-
-  const{data:users=[],isLoading,refetch}=useQuery({
-    queryKey:['users',user?.email],
-    queryFn:async()=>{
-      const{data}=await axiosSecure.get(`/all-users/${user?.email}`)
+  const {
+    data: users = [],
+    isLoading,
+    refetch,
+  } = useQuery({
+    queryKey: ["users", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/all-users/${user?.email}`);
       return data;
-    }
+    },
   });
 
-  console.log(users)
+  console.log(users);
 
-  if(isLoading){
-    return <LoadingSpinner></LoadingSpinner>
+  if (isLoading) {
+    return <LoadingSpinner></LoadingSpinner>;
   }
   return (
     <>
-      <div className='container mx-auto px-4 sm:px-8'>
+      <div className="container mx-auto px-4 sm:px-8">
         <Helmet>
           <title>Manage Users</title>
         </Helmet>
-        <div className='py-8'>
-          <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
-            <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
-              <table className='min-w-full leading-normal'>
+        <div className="py-8">
+          <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+            <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+              <table className="min-w-full leading-normal">
                 <thead>
                   <tr>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Email
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Role
                     </th>
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Status
                     </th>
 
                     <th
-                      scope='col'
-                      className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                      scope="col"
+                      className="px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal"
                     >
                       Action
                     </th>
                   </tr>
                 </thead>
                 <tbody>
-                  {users.map((user,i)=> <UserDataRow user={user} refetch={refetch} key={user?._id} />)}
+                  {users.map((user, i) => (
+                    <UserDataRow
+                      user={user}
+                      refetch={refetch}
+                      key={user?._id}
+                    />
+                  ))}
                 </tbody>
               </table>
             </div>
@@ -72,7 +81,7 @@ const ManageUsers = () => {
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default ManageUsers
+export default ManageUsers;
